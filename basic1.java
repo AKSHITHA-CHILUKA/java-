1e713c75dd4a0b68652c163727b8cbdb0bf10877
~Chiluka Akshitha <120377576+AKSHITHA-CHILUKA@users.noreply.github.com>
2024-05-28 19:41:15 +0530

    Create basic1.java
    basics of java using variables and datatypes

---
+++ basic1.java
@@ -0,0 +1,9 @@
+class firstclass {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        String name ="tommy ";
+        System.out.println(a+b);
+        System.out.println(name);
+    }
+}
